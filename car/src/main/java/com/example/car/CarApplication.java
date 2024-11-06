9e10329e9df06094972265028232e4c4b6ee8f18
safae oujri <Safa>
2024-11-06 20:31:41 +0100

    first commit

--- car/src/main/java/com/example/car/CarApplication.java
+++ car/src/main/java/com/example/car/CarApplication.java
@@ -2,11 +2,14 @@ package com.example.car;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CarApplication {
 
     public static void main(String[] args) {
